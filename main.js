842704579d9c9d12045996b1813ece9dc48c447e
BenjaminZhai <99426763+BenjaminZhai@users.noreply.github.com>
2022-12-01 13:56:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -36,28 +36,64 @@ function goBtnClicked() {
 function allColors() {
   // Display Name and Family of All Colors
   outputEl.innerHTML = "<h3>Display All Colors</h3>";
-  for (i = 0; i < family.length; i++) {
-    outputEl.innerHTML = family[i];
+  let outputstr = ''
+  for (i = 0; i < colorData.length; i++) {
+    outputstr += `<div> <h3> ${colorData[i].name} <br>${colorData[i].family}</h3> `
     
   }
+  outputEl.innerHTML = outputstr
 }
 
 function brightColors() {
   // Display Name and Brightness of All Colors with a Brightness of 200 and Higher
   outputEl.innerHTML = "<h3>Display Bright Colors</h3>";
+  let outputstr = ''
+  for (i = 0; i < colorData.length; i++) {
+    if(colorData[i].brightness >= 200){
+      outputstr += `<div> <h3> ${colorData[i].name} <br>${colorData[i].brightness}</h3> `
+  }
+    }
+  outputEl.innerHTML = outputstr
 }
 
 function redPinkFamilies() {
   // Count Colors in Red/Pink Families
   outputEl.innerHTML = "<h3>Count Red/Pink Family Colors</h3>";
+  let outputstr = ''
+  for (i = 0; i < colorData.length; i++) {
+    if(colorData[i].family === 'Red' || colorData[i].family === 'Pink'){
+      outputstr += `<div> <h3> ${colorData[i].name} <br>${colorData[i].family}</h3> `
+  }
+    }
+  outputEl.innerHTML = outputstr
 }
 
 function familySearch() {
   // Display Name and Family of all Colors that Match a User Provided Family Name. Also Output a Count of Colors Found.
   outputEl.innerHTML = "<h3>Family Search</h3>";
+  let x = prompt('Please Input A Color Family')
+  let outputstr = ''
+  let count = 0
+  for (i = 0; i < colorData.length; i++) {
+    if(colorData[i].family === x){
+      count++
+      outputstr += `<div> <h3> ${colorData[i].name} <br>${colorData[i].family}</h3> `
+  }
+    }
+  outputEl.innerHTML = count + outputstr
 }
 
 function startLetterSearch() {
   // Display Name of all Colors that Match a User Provided Starting Letter. Also Output a Count of Colors Found.
   outputEl.innerHTML = "<h3>Start Letter Search</h3>";
+  let x = prompt('Please Input A First Letter')
+  let outputstr = ''
+  let count = 0
+  for (i = 0; i < colorData.length; i++) {
+    if(colorData[i].name.charAt(0) === x){
+      count++
+      outputstr += `<div> <h3> ${colorData[i].name} <br></h3> `
+  }
+    }
+  outputEl.innerHTML = count + outputstr
 }
